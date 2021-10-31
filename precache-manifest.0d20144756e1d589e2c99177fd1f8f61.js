25f2eabf425c9d19d7014e7f4834465edb20c596
rodrigo <rodrigo.quezada.w@hotmail.com>
2021-10-31 18:36:21 -0300

    deployprueba

---
+++ precache-manifest.0d20144756e1d589e2c99177fd1f8f61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ea746eca388b07e7ed",
+    "url": "/css/chunk-vendors.3a280c32.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "1f974d7fc3939b6fbf1ce7d39c42632e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa02e840335f23a6efbb",
+    "url": "/js/about.ceaf121e.js"
+  },
+  {
+    "revision": "44bffe0821c5b1729038",
+    "url": "/js/app.ed459a56.js"
+  },
+  {
+    "revision": "75ea746eca388b07e7ed",
+    "url": "/js/chunk-vendors.c4d06604.js"
+  },
+  {
+    "revision": "c6d4192c9513ac988eacf0b96d86cf5c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
